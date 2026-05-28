25128c411bccfcc176975954b500aff96dd3d2d3
Dov Benyomin Sohacheski <b@kloud.email>
2026-05-28 16:10:38 +0300

    ✨ Render prepackaged-extensions table from extensions.yaml (#142)

--- scripts/generate-all.mjs
+++ scripts/generate-all.mjs
@@ -3,3 +3,4 @@ import { execSync } from "node:child_process"
 execSync("node ./scripts/generate-dependencies.mjs", { stdio: "inherit" })
 execSync("node ./scripts/generate-deprecated-variables.mjs", { stdio: "inherit" })
 execSync("node ./scripts/generate-environment-variables.mjs", { stdio: "inherit" })
+execSync("node ./scripts/generate-extensions.mjs", { stdio: "inherit" })
